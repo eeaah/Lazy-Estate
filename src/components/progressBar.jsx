73ebc83141fadfcacb4a4b4ac2a546be57199212
IanPrigmore <ianaprigmore@gmail.com>
2023-11-05 11:07:19 -0600

    Progress bar fix

--- src/components/progressBar.jsx
+++ src/components/progressBar.jsx
@@ -13,78 +13,72 @@ import { LoadingOutlined,
 import { Steps } from 'antd';
 import Forms from '../main.jsx'
 
-export default function ProgressBar(array) {
+export default function ProgressBar({array}) {
     var statusArr = []
     var skipped = false;
-    console.log(array)
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < 9; i++) {
       if(array[i] != null && !skipped) {
         statusArr.push('finish');
-        console.log(array[i])
-        console.log(statusArr[i])
       } else if (array[i] != null) {
         statusArr.push('process');
-        console.log(array[i])
-        console.log(statusArr[i])
       } else {
         statusArr.push('wait');
         skipped = true;
-        console.log(array[i])
-        console.log(statusArr[i])
       }
     }
+    if (!array.includes(null)) statusArr.push('finish');
     let itemsArr = [
-        {
-          status: statusArr[0],
-          icon: <DollarOutlined />,
-          // step: 1,
-        },
-        {
-          status: 'process',
-          icon: <ContainerOutlined />,
-          // step: 2,
-        },
-        {
-          status: 'wait',
-          icon: <CarOutlined />,
-          // step: 3,
-        },
-        {
-          status: 'wait',
-          icon: <BankOutlined />,
-          // step: 4,
-        },
-        {
-          status: 'wait',
-          icon: <HomeOutlined />,
-          // step: 5,
-        },
-        {
-          status: 'wait',
-          icon: <ArrowDownOutlined />,
-          // step: 6,
-        },
-        {
-          status: 'wait',
-          icon: <BankOutlined />,
-          // step: 7,
-        },
-        {
-          status: 'wait',
-          icon: <HomeOutlined />,
-          // step: 8,
-        },
-        {
-          status: 'wait',
-          icon: <CreditCardOutlined />,
-          // step: 9,
-        },
-        {
-          status: 'wait',
-          icon: <CloseOutlined />,
-          // step: 10,
-        },
-      ]
+      {
+        status: statusArr[0],
+        icon: <DollarOutlined />,
+        // step: 1,
+      },
+      {
+        status: statusArr[1],
+        icon: <ContainerOutlined />,
+        // step: 2,
+      },
+      {
+        status: statusArr[2],
+        icon: <CarOutlined />,
+        // step: 3,
+      },
+      {
+        status: statusArr[3],
+        icon: <BankOutlined />,
+        // step: 4,
+      },
+      {
+        status: statusArr[4],
+        icon: <HomeOutlined />,
+        // step: 5,
+      },
+      {
+        status: statusArr[5],
+        icon: <ArrowDownOutlined />,
+        // step: 6,
+      },
+      {
+        status: statusArr[6],
+        icon: <BankOutlined />,
+        // step: 7,
+      },
+      {
+        status: statusArr[7],
+        icon: <HomeOutlined />,
+        // step: 8,
+      },
+      {
+        status: statusArr[8],
+        icon: <CreditCardOutlined />,
+        // step: 9,
+      },
+      {
+        status: statusArr[9],
+        icon: <CheckOutlined />,
+        // step: 10,
+      },
+    ];
 
     return (
       <Steps
